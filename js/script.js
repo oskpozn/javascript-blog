44a2094d6143a6b6c1d734a76afdbc8796863606
Oskar Pozniak <oskpozn@gmail.com>
2019-10-06 12:34:01 +0200

    added remove.activeclass to js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,15 @@
     console.log(event)
   
     /* remove class 'active' from all article links  */
-  
+  const activeLinks = document.querySelectorAll('.titles a.active')
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active')
+    }
     /* add class 'active' to the clicked link */
-  
+    const activeArticles = document.querySelectorAll('.titles article.active')
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active')
+    }
     /* remove class 'active' from all articles */
   
     /* get 'href' attribute from the clicked link */
@@ -18,6 +24,7 @@
     /* find the correct article using the selector (value of 'href' attribute) */
   
     /* add class 'active' to the correct article */
+    
   }
   
   const links = document.querySelectorAll('.titles a');
